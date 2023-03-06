119c58904c7a4765918e0f91c4f2f59a92e21aec
Jason <jasondal91@gmail.com>
2023-03-06 19:27:28 +0900

    add copy typedArr

--- javascript/deep_clone_object/src/copy_object.final.js
+++ javascript/deep_clone_object/src/copy_object.final.js
@@ -5,7 +5,25 @@ const isSet = (value) => value instanceof Set ?? false;
 const isMap = (value) => value instanceof Map ?? false;
 const isObject = (value) => value instanceof Object ?? false;
 
-//  TypedArray
+const typedArrayViewLists = [Int8Array, Int16Array, Int32Array];
+
+const isTypedArray = (value) => {
+  for (const view of typedArrayViewLists) {
+    if (value instanceof view) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const getTypedArrayView = (value) => {
+  for (const View of typedArrayViewLists) {
+    if (value instanceof View) {
+      return View;
+    }
+  }
+  return null;
+};
 
 function copyDate(value) {
   return new Date(value.getTime());
@@ -30,6 +48,11 @@ function copySet(value) {
 function copyMap(value) {
   const mapToArr = [...value];
   return new Map(copyArray(mapToArr));
+}
+
+function copyTypedArray(value) {
+  const View = getTypedArrayView(value);
+  return new View(value);
 }
 
 function copyObject(value) {
@@ -60,6 +83,10 @@ const copyTable = [
   {
     validation: isMap,
     copy: copyMap,
+  },
+  {
+    validation: isTypedArray,
+    copy: copyTypedArray,
   },
   {
     validation: isObject,
--- javascript/deep_clone_object/src/test/copy_object.final.test.js
+++ javascript/deep_clone_object/src/test/copy_object.final.test.js
@@ -78,16 +78,10 @@ describe('copy object', () => {
   });
 
   it('returns deep copy of typedArray', () => {
-    const buffer = new ArrayBuffer(8);
-    const typedArray = new Int8Array(buffer);
-    const typedArray1 = new Int8Array(typedArray);
-    const typedArray2 = new Int16Array(buffer);
-    const typedArray3 = new Int8Array(typedArray2);
-    typedArray[0] = 32;
+    const typedArray = new Int8Array(8);
 
-    console.log(typedArray1);
-    console.log(typedArray);
-    console.log(typedArray2);
-    console.log(typedArray3 instanceof Int16Array);
+    const deepCopiedTypedArray = deepCopy(typedArray);
+
+    expect(deepCopiedTypedArray).not.toBe(typedArray);
   });
 });
